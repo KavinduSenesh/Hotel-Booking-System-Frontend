566e79f693ae9eb7fb9dd39944517a60d5dde2ff
Kavindu Senesh <ksenesh5@gmail.com>
2025-03-13 22:27:20 +0530

    Add Bootstrap for styling; enhance Navbar with dropdown and navigation links

--- hotel-booking-system-frontend/src/components/layout/Navbar.jsx
+++ hotel-booking-system-frontend/src/components/layout/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return(
@@ -13,13 +14,57 @@ const Navbar = () => {
                     type={"button"}
                     data-bs-toggle="collapse"
                     data-bs-target="#navbarScroll"
-                    arial-controls={"navbarScroll"}
+                    aria-controls={"navbarScroll"}
                     aria-expanded={"false"}
                     aria-label={"Toggle navigation"}
                 >
-                    <span className={"navbar-toggler-icon"}>
-                    </span>
+                    <span className={"navbar-toggler-icon"}></span>
                 </button>
+                <div className={"collapse navbar-collapse"} id={"navbarScroll"}>
+                    <ul className={"navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll"}>
+                        <li className={"nav-item"}>
+                            <NavLink className={"nav-link"}  to={"/browse-all-rooms"}>
+                                Browse all rooms
+                            </NavLink>
+                        </li>
+                        <li className={"nav-item"}>
+                            <NavLink to={"/manage-all-rooms"} className={"nav-link"} >
+                                Manage rooms
+                            </NavLink>
+                        </li>
+                    </ul>
+
+                    <ul className={"d-flex navbar-nav"}>
+                        <li className={"nav-item"}>
+                            <NavLink className={"nav-link"} to={"/find-booking"}>
+                                Find My Booking
+                            </NavLink>
+                        </li>
+
+                        <li className={"nav-item dropdown"}>
+                            <a className={"nav-link dropdown-toggle"} href={"#"} id={"navbarDropdown"} role={"button"} data-bs-toggle={"dropdown"} aria-expanded={"false"}>
+                                Account
+                            </a>
+                            <ul className={"dropdown-menu"} aria-labelledby={"navbarDropdown"}>
+                                <li>
+                                    <Link to={"/login"} className={"dropdown-item"}>
+                                        Login
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={"/profile"} className={"dropdown-item"}>
+                                        Profile
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={"/logout"} className={"dropdown-item"}>
+                                        Logout
+                                    </Link>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
