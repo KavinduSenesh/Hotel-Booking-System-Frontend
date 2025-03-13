5ec76894898008a21910dfebe66701fb5f162a73
Kavindu Senesh <ksenesh5@gmail.com>
2025-03-13 22:27:04 +0530

    Add Navbar and Footer components; implement fixed footer layout and responsive design

--- hotel-booking-system-frontend/src/App.jsx
+++ hotel-booking-system-frontend/src/App.jsx
@@ -6,6 +6,13 @@ import ExistingRooms from './components/room/ExistingRooms';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./components/home/Home.jsx";
 import EditRoom from "./components/room/EditRoom.jsx";
+import Navbar from "./components/layout/Navbar.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import { createPopper } from "@popperjs/core";
+import Footer from "./components/layout/Footer.jsx";
+window.Popper = { createPopper };
+
 
 function App() {
 
@@ -13,6 +20,7 @@ function App() {
     <>
       <main>
           <Router>
+              <Navbar/>
               <Routes>
                   <Route path={"/"} element={<Home />} />
                   <Route path={"/edit-rooms/:roomId"} element={<EditRoom />} />
@@ -20,6 +28,7 @@ function App() {
                   <Route path={"/add-room"} element={<AddRoom />} />
               </Routes>
           </Router>
+          <Footer/>
       </main>
     </>
   )
--- hotel-booking-system-frontend/src/components/layout/Footer.jsx
+++ hotel-booking-system-frontend/src/components/layout/Footer.jsx
@@ -1,22 +1,25 @@
 import React from "react";
-import {Col, Container, Row} from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 
 const Footer = () => {
     let today = new Date();
 
     return (
-        <div>
-            <footer>
-                <Container className={"btn-dark text-light py-3 footer mt-lg-5"}>
-                    <Row>
-                        <Col xs={12} md={12} className={"text-center"}>
-                            <p>&copy; {today.getFullYear()} Mount Hotel</p>
-                        </Col>
-                    </Row>
-                </Container>
-            </footer>
-        </div>
-    )
-}
+        <footer className="bg-dark text-light py-3 text-center" style={{
+            position: "fixed",
+            bottom: 0,
+            width: "100%",
+            zIndex: 1000
+        }}>
+            <Container>
+                <Row>
+                    <Col xs={12} md={12} className="text-center">
+                        <p>&copy; {today.getFullYear()} Mount Hotel</p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    );
+};
 
 export default Footer;
