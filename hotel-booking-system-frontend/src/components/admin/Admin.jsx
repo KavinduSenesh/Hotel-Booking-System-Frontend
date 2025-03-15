2f965164f7c8d15e4792bfc8d30db788021d83f9
Kavindu Senesh <ksenesh5@gmail.com>
2025-03-15 20:46:25 +0530

    Add RoomCarousel component; update Admin and Home layouts to include room carousel

--- hotel-booking-system-frontend/src/components/admin/Admin.jsx
+++ hotel-booking-system-frontend/src/components/admin/Admin.jsx
@@ -3,13 +3,13 @@ import {Link} from "react-router-dom";
 
 const Admin = () => {
     return(
-        <Section className={"container mt-5"}>
+        <section className={"container mt-5"}>
             <h2>Welcome to Admin panel</h2>
             <hr/>
             <Link to={"/add-room"}>
                 Manage Rooms
             </Link>
-        </Section>
+        </section>
     )
 }
 
--- hotel-booking-system-frontend/src/components/home/Home.jsx
+++ hotel-booking-system-frontend/src/components/home/Home.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import MainHeader from "../layout/MainHeader.jsx";
 import HotelService from "../common/HotelService.jsx";
 import Parallax from "../common/Parallax.jsx";
+import RoomCarousel from "../common/RoomCarousel.jsx";
 
 const Home = () => {
     return (
         <section>
             <MainHeader/>
                 <section className={"container"}>
+                    <RoomCarousel/>
                     <Parallax/>
+                    <RoomCarousel/>
                     <HotelService/>
                     <Parallax/>
+                    <RoomCarousel/>
                 </section>
         </section>
     )
